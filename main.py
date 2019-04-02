7ccfbf97c09ea52985f58c116b3f43134404ab24
Scott Henderson <henders999@gmail.com>
2019-04-01 23:29:10 -0700

    Removed TODO comments. Commented out loss print statement during training.

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name      = 'image_input:0'
     vgg_keep_prob_tensor_name  = 'keep_prob:0'
@@ -138,8 +136,6 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-
     # The output tensor is 4D so we have to reshape it to 2D:
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     # logits is now a 2D tensor where each row represents a pixel and each column represents a class
@@ -176,12 +172,13 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         batch_num = 0
         for image, label in get_batches_fn(batch_size):
             batch_num += 1
-            _, loss = sess.run([train_op, cross_entropy_loss],
-                               feed_dict={ input_image: image,
-                                           correct_label: label,
-                                           keep_prob: KEEP_PROB,
-                                           learning_rate: LEARNING_RATE })
-            print('Batch {:2} loss: {}'.format(batch_num, loss))
+            # test, loss = sess.run([train_op, cross_entropy_loss],
+            sess.run([train_op, cross_entropy_loss],
+                     feed_dict={ input_image: image,
+                                 correct_label: label,
+                                 keep_prob: KEEP_PROB,
+                                 learning_rate: LEARNING_RATE })
+            # print('Batch {:2} loss: {}'.format(batch_num, loss))
 tests.test_train_nn(train_nn)
 
 
@@ -244,7 +241,6 @@ def run():
         Existing implementaion labels red pixesl as 'false', everything else is 'true'.
         Modify to use an integer class, e.g., red==0, black==1, pink/other=2
         """
-
 
 if __name__ == '__main__':
     run()
